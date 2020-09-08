c95dd5459ebfa7fbe83498a93dc968292a3b4e3b
Malin Henningsson <malinhenningsson@live.se>
2020-09-08 11:10:56 +0200

    Added custom useFetch hook.

--- src/components/LatestNews.js
+++ src/components/LatestNews.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useFetch from '../hooks/useFetch';
 
 const LatestNews = () => {
+    let url = 'http://newsapi.org/v2/top-headlines?' +
+            'country=se&' +
+            `apiKey=${process.env.REACT_APP_NEWSAPI_APIKEY}`;
+    useFetch(url)
+
     return (
         <div>
             <h1>Latest News</h1>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+const useFecth = (initialUrl) => {
+    const [url, setUrl] = useState(initialUrl);
+    const [data, setData] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if(!url) {
+                return;
+            }
+            console.log('Fetching data with url: ', url);
+            setIsLoading(true);
+
+            try {
+                const res = await fetch(url);
+                const data = await res.json();
+                console.log(data);
+                setData(data);
+            } catch (err) {
+                console.error(err);
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+
+    }, [url]);
+
+    return [data, isLoading, error, setUrl];
+}
+ 
+export default useFecth;
